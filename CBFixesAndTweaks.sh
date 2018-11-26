0ee9e5f4e6f02aeeaafd7bfea112558239bdcd8f
Kyle Norquist <knorquist@gmail.com>
2018-11-26 12:12:59 -0800

    Removed audio tweaks

--- CBFixesAndTweaks.sh
+++ CBFixesAndTweaks.sh
@@ -1,24 +1,10 @@
 #!/bin/bash
 #
-# Script to Fix and apply Tweaks to Chromebooks running Linux
+# Script to fix key mappings for Toshiba Chromebook 2 (2015), Gandof (The i3 version)
 #
-
-# get CB model codename
-CBCODENAME=$(sudo dmidecode -s system-product-name)
-
-## functions ##
-fix_sound() {
-  ## for Baytrail TCB2
-  # stop sound service
-  sudo alsa force-unload
-  
-  # make backup of original sound.state file
-  sudo cp -n /var/lib/alsa/asound.state /var/lib/alsa/asound.state.bck
-  
-  # copy new sound.state file
-  sudo cp asound.state /var/lib/alsa
-}
-
+# Based of fascinatingcaptain's original script, but minus the audio tweaks since
+# Gandof audio works out-of-the box with the new kernel.
+#
 fix_keyboard_keys() {
 	# make backup of original pc config file
 	sudo cp -n /usr/share/X11/xkb/symbols/pc /usr/share/X11/xkb/symbols/pc.bck
@@ -30,14 +16,6 @@ fix_keyboard_keys() {
 	sudo rm -rf /var/lib/xkb/*
 }
 ## ..functions ##
-
-# check for Baytrail CBs
-if [ "$CBCODENAME" == "Swanky" ]
-then
-	fix_sound
-fi
-echo "*******************************************************"
-echo "Fixed sound for Baytrail Chromebook"
 
 # apply keyboard remapping
 fix_keyboard_keys
